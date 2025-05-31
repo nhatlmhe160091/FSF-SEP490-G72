938dcf931f23abb9ae6b84d1d24f9e446ddf32a2
nhatlmhe160091 <nhatlmhe160091@fpt.edu.vn>
2025-06-01 00:00:49 +0700

    fix config

--- be/src/controllers/index.js
+++ be/src/controllers/index.js
@@ -1,6 +1,10 @@
 const UserController = require('./user.controller');
 const GuestController = require('./guest.controller');
+const TypeController = require('./type.controller');
+const SportFieldController = require('./sportField.controller');
 module.exports = {
     GuestController,
     UserController,
+    TypeController,
+    SportFieldController
 }
--- be/src/middlewares/index.js
+++ be/src/middlewares/index.js
@@ -1,6 +1,8 @@
 const AuthMiddleware = require('./auth.middleware');
 const ErrorHandle = require('./errorHandle.middleware');
+const cloudinaryFileMiddleware = require('./cloudinary.middleware');
 module.exports = {
     AuthMiddleware,
     ErrorHandle,
+    cloudinaryFileMiddleware
 }
