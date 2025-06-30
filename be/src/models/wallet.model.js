d9dfc82171936fadb11c81fa9e3878070c20fc86
viet30072001 <vietddhe151061@fpt.edu.vn>
2025-06-30 16:01:32 +0700

    wallet

---
+++ be/src/models/wallet.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const walletSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  balance: {
+    type: Number,
+    default: 0,
+    min: 0
+  },
+  currency: {
+    type: String,
+    default: 'VND'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Wallet', walletSchema);
---
+++ be/src/services/walletTransaction.service.js
@@ -0,0 +1,8 @@
+const WalletTransaction = require('../models/walletTransaction.model');
+const Wallet = require('../models/wallet.model');
+
+const WalletTransactionService = {
+
+}
+
+module.exports = WalletTransactionService;
