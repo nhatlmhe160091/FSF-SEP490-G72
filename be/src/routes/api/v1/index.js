701f1cf4452d2122a16f0fd0e6478c15e21314b9
viet30072001 <vietddhe151061@fpt.edu.vn>
2025-07-08 23:00:18 +0700

    update crud view news

--- be/src/routes/api/v1/index.js
+++ be/src/routes/api/v1/index.js
@@ -9,5 +9,6 @@ router.use('/guest', guestRouter);
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
 router.use('/sport-field', sportFieldRouter);
+router.use('/news', newsRouter);
 
 module.exports = router;
