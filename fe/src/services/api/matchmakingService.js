3161391ed6f8964bae17304e23d0deaae7423194
Thai12341 <thaikvhe160086@fpt.edu.vn>
2025-07-16 11:46:39 +0700

    matching maintance euiment fix

---
+++ fe/src/services/api/matchmakingService.js
@@ -0,0 +1,31 @@
+import api from '../index';
+import { handleApiCall } from '../../utils/handleApi';
+
+export const matchmakingService = {
+    getAllMatchmakings: async () => {
+        return handleApiCall(() => api.get('/matchmaking'));
+    },
+    
+    getMatchmakingById: async (id) => {
+        return handleApiCall(() => api.get(`/matchmaking/${id}`));
+    },
+    
+    createMatchmaking: async (data) => {
+        return handleApiCall(() => api.post('/matchmaking', data));
+    },
+    
+    updateMatchmaking: async (id, data) => {
+        return handleApiCall(() => api.put(`/matchmaking/${id}`, data));
+    },
+    
+    deleteMatchmaking: async (id) => {
+        return handleApiCall(() => api.delete(`/matchmaking/${id}`));
+    },
+    getOpenMatchmakings: async () => {
+        return handleApiCall(() => api.get('/matchmaking/open'));
+    },
+    joinMatchmaking: async (id, representativeId) => {
+        return handleApiCall(() => api.post(`/matchmaking/${id}/join`, { representativeId }));  
+    },
+    };
+export default matchmakingService;
