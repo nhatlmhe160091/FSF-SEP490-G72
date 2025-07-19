455fa29f1bde81c94d883c28c3604c1cece78873
Bui Tien Anh <anhbthe170035@fpt.edu.vn>
2025-07-19 18:12:04 +0700

    update be category policy

--- be/src/routes/api/v1/index.js
+++ be/src/routes/api/v1/index.js
@@ -4,10 +4,14 @@ const guestRouter = require('./guest.router');
 const userRouter = require('./user.router');
 const typeRouter = require('./type.router');
 const sportFieldRouter = require('./sportField.router');
+const categoryPolicyRouter = require('./categoryPolicy.router');
+const policyRouter = require('./policy.router');
 
 router.use('/guest', guestRouter);
 router.use('/user', userRouter);
 router.use('/type', typeRouter);
 router.use('/sport-field', sportFieldRouter);
+router.use('/category-policy', categoryPolicyRouter);
+router.use('/policy', policyRouter);
 
 module.exports = router;
