8ddbb37f223cf3b944d8dd5de3950a5dca902323
VsDuy <136254073+VsDuy@users.noreply.github.com>
2025-11-19 22:53:45 +0700

    update header

---
+++ fe/src/components/headers/staffHeader.jsx
@@ -0,0 +1,92 @@
+// ManagerHeader.jsx
+import * as React from 'react';
+import {
+  AppBar, Box, Toolbar, Typography, Menu, Container,
+  Tooltip, MenuItem, Link
+} from '@mui/material';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import Person3Icon from '@mui/icons-material/Person3';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/authContext';
+import ProfileDialog from '../dialogs/profileDialog';
+import Notification from '../../components/notifications/Notification';
+function StaffHeader() {
+  const navigate = useNavigate();
+  const { currentUser, isUserLoggedIn } = useAuth();
+  const [user, setUser] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [openProfileDialog, setOpenProfileDialog] = React.useState(false);
+
+  const handleOpenUserMenu = (event) => setAnchorElUser(event.currentTarget);
+  const handleCloseUserMenu = () => setAnchorElUser(null);
+  const handleSignOut = () => navigate('/sign-out');
+
+  React.useEffect(() => {
+    if (isUserLoggedIn) {
+      setUser(currentUser);
+    }
+  }, [currentUser, isUserLoggedIn]);
+
+  return (
+    <>
+      <AppBar position="fixed" sx={{ backgroundColor: '#e8801eff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <SportsSoccerIcon onClick={() => navigate('/staff')} sx={{ mr: 2, cursor: 'pointer' }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/staff"
+              sx={{
+                mr: 2,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.2rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              SPORT FIELD STAFF
+            </Typography>
+
+            <Box sx={{ flexGrow: 1 }} />
+             <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                <Notification />
+              <Tooltip title="Thông tin tài khoản">
+                <Box sx={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={handleOpenUserMenu}>
+                  <Person3Icon />
+                  <Typography sx={{ ml: 1, fontWeight: 600 }}>
+                    {user ? `Nhân viên: ${user.lname} ${user.fname}` : 'Nhân viên'}
+                  </Typography>
+                </Box>
+              </Tooltip>
+
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                keepMounted
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem onClick={() => { handleCloseUserMenu(); setOpenProfileDialog(true); }}>
+                  <Typography textAlign="center">Thông tin tài khoản</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleSignOut}>
+                  <Typography textAlign="center">Đăng xuất</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <ProfileDialog open={openProfileDialog} setOpen={setOpenProfileDialog} />
+    </>
+  );
+}
+
+export default StaffHeader;
