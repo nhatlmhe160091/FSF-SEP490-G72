b7b1fbcbc73ef5e4d5aeb92bdcfb305c60886896
nhatlmhe160091 <nhatlmhe160091@fpt.edu.vn>
2025-11-23 23:34:23 +0700

    feedback for complex chung

--- be/src/controllers/feedback.controller.js
+++ be/src/controllers/feedback.controller.js
@@ -72,6 +72,15 @@ class FeedbackController {
             res.status(500).send(error.message);
         }
     }
+
+    async getFeedbackSummaryByComplex(req, res) {
+        try {
+            const summary = await FeedbackService.getFeedbackSummaryByComplex(req.params.complexId);
+            res.status(200).json(summary);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    }
 }
 
 module.exports = new FeedbackController();
